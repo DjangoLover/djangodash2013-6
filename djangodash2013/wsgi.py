025d38b56c68957f0b54bdbd0a426e18e649076c
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2013-09-29 23:34:02 +0200

    removed last bits of Heroku production setup

--- djangodash2013/wsgi.py
+++ djangodash2013/wsgi.py
@@ -19,6 +19,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangodash2013.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
